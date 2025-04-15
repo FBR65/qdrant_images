a486f17e30c865dee5b70024c0a08d93e48c29c2
FBR65 <115529371+FBR65@users.noreply.github.com>
2025-04-15 20:45:09 +0200

    20250415

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-** Bildersuche per Qdrant
+# Image Research with Qdrant
+
+Use Qdrant Vector DB to compare uploaded Images with Images inside Qdrant.
+
+Two approaches were considered: RGB images and black and white images.
--- hf_downloader.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-
-#snapshot_download(repo_id="openai/clip-vit-base-patch32",local_dir="/home/reifr1z/models/openai/clip-vit-base-patch32")
-
-snapshot_download(repo_id="laion/CLIP-ViT-B-32-laion2B-s34B-b79K",local_dir="/home/reifr1z/models/laion/CLIP-ViT-B-32-laion2B-s34B-b79K")
-
